dc18d34a595631968853a37801686290fa6d7d4b
Gabriel Augusto Paz Guedez <gabriel_paz21@hotmail.com>
2024-06-03 17:21:18 -0400

    Update dependencies and refactor batch configuration
    Updated project dependencies in the pom.xml and replaced @Autowired with constructor-based dependency injection in BatchConfig class. Also refactored Spring Batch configuration with the new StepBuilder and JobBuilder classes for better clarity and manageability. Made CustomerProcessor method param non-null for safety.

--- 079. Spring Batch/spring-batch/src/main/java/com/example/config/BatchConfig.java
+++ 079. Spring Batch/spring-batch/src/main/java/com/example/config/BatchConfig.java
@@ -6,10 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
@@ -17,37 +17,42 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableBatchProcessing
 public class BatchConfig {
 
     private static final Logger LOG = LoggerFactory.getLogger(BatchConfig.class);
 
-    @Autowired
-    public JobBuilderFactory jobBuilderFactory;
+    public final DataSource dataSource;
 
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
+    private final JdbcTemplate jdbcTemplate;
 
-    @Autowired
-    public DataSource dataSource;
+    private final JobRepository jobRepository;
+
+    private final PlatformTransactionManager platformTransactionManager;
 
     @Value("${app.file.input}")
     private String customersFile;
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    public BatchConfig(DataSource dataSource,
+                       JdbcTemplate jdbcTemplate,
+                       JobRepository jobRepository,
+                       PlatformTransactionManager platformTransactionManager) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+        this.jobRepository=jobRepository;
+        this.platformTransactionManager=platformTransactionManager;
+    }
 
-    // lectura del customers.csv
+    // reading customers.csv
     @Bean
     public FlatFileItemReader<Customer> reader() {
         return new FlatFileItemReaderBuilder<Customer>()
@@ -58,11 +63,16 @@ public class BatchConfig {
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<>() {{
                     setTargetType(Customer.class);
                 }})
-                .linesToSkip(1) // salta la primera linea del csv que es la cabecera
+                .linesToSkip(1) // skips the first line of the CSV, which is the header
                 .build();
     }
 
-    // carga de datos en la tabla customers
+    @Bean
+    CustomerProcessor processor() {
+        return new CustomerProcessor();
+    }
+
+    // loading data into customers table
     @Bean
     public JdbcBatchItemWriter<Customer> writer(){
         return new JdbcBatchItemWriterBuilder<Customer>()
@@ -79,7 +89,7 @@ public class BatchConfig {
 
     @Bean
     public Job job1(){
-        return jobBuilderFactory.get("job1")
+        return new JobBuilder("job1", jobRepository)
                 .incrementer(new RunIdIncrementer())
 //                .listener(customerListener())
                 .start(step1())
@@ -88,39 +98,36 @@ public class BatchConfig {
                 .build();
     }
 
-    // Borrar los datos de la tabla customers
+    // Delete data from the customer table
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
+        return new StepBuilder("step1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
-                    LOG.info("Borrando los datos de la tabla customers");
+                    LOG.info("Deleting data from the customers table");
                     jdbcTemplate.update("DELETE FROM customers");
                     return RepeatStatus.FINISHED;
-                })
+                },platformTransactionManager)
+                .build();
+    }
+
+    // read customers and insert into customer table
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .<Customer, Customer> chunk(10, platformTransactionManager)
+                .reader(reader()) // read csv
+                .processor(processor()) // process
+                .writer(writer()) // save to db
                 .build();
     }
 
     @Bean
-    CustomerProcessor processor() {
-        return new CustomerProcessor();
-    }
-    // leer customers e insertar en la tabla customers
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .<Customer, Customer> chunk(10)
-                .reader(reader()) // leer csv
-                .processor(processor()) // procesar
-                .writer(writer()) // guardar en db
-                .build();
-    }
-    @Bean
     public Step step3() {
-        return stepBuilderFactory.get("step2")
-                .<Customer, Customer> chunk(10)
-                .reader(reader()) // leer csv
-                .processor(processor()) // procesar
-                .writer(writer()) // guardar en db
+        return new StepBuilder("step3", jobRepository)
+                .<Customer, Customer> chunk(10, platformTransactionManager)
+                .reader(reader()) // read csv
+                .processor(processor()) // process
+                .writer(writer()) // save to db
                 .build();
     }
 
--- 079. Spring Batch/spring-batch/src/main/java/com/example/processor/CustomerProcessor.java
+++ 079. Spring Batch/spring-batch/src/main/java/com/example/processor/CustomerProcessor.java
@@ -1,17 +1,17 @@
 package com.example.processor;
 
-import com.example.config.BatchConfig;
 import com.example.model.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.lang.NonNull;
 
 public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+
     private static final Logger LOG = LoggerFactory.getLogger(CustomerProcessor.class);
 
-
     @Override
-    public Customer process(Customer customer) throws Exception {
+    public Customer process(@NonNull Customer customer) {
         LOG.info("Processing customer: {}", customer);
         customer.setFirstName(customer.getFirstName().toUpperCase());
         return customer;
